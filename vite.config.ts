e3aa4fcbb73da737688482db98a7dbcdb3ce6fc7
ziemekrokosz-source <ziemekrokosz@gmail.com>
2026-02-16 10:07:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Pozwala na używanie process.env.API_KEY bezpośrednio w kodzie, 
+    // co jest wymagane przez instrukcję obsługi SDK.
+    'process.env': {
+      API_KEY: JSON.stringify(process.env.API_KEY)
+    }
+  }
 });
